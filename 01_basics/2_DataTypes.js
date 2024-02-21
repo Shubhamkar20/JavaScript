c3839e3874ad5657d32229d866c678c2e5adfa45
Shubhamkar20 <149138866+Shubhamkar20@users.noreply.github.com>
2024-02-21 14:32:37 +0000

    modified

---
+++ 01_basics/2_DataTypes.js
@@ -0,0 +1,27 @@
+// let age = 20;
+
+// console.log(typeof(age));
+
+/* Now convert the age into string
+ let ageToString = String(age);
+ console.log(typeof(ageToString));
+*/
+
+
+
+// Now converting a string into number
+let fname = '20'
+console.log('Type of fname : '+typeof fname);
+
+// convert it into number
+// let fnameToNumber = Number(fname);
+
+// what if we change value of fname to '20a' what it will print.
+fname = '20a';
+let fnameToNumber = Number(fname);
+console.log('now type of fname :'+ typeof fnameToNumber);
+console.log(fnameToNumber); // it returns NaN
+
+
+
+
