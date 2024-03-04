c86e0c628b191f80c320c7ef40028f9fa8e80ed0
Shubhamkar Sambhaji Patil <149138866+Shubhamkar20@users.noreply.github.com>
2024-03-04 04:23:19 +0000

    modified

---
+++ 15_scope2_hoisting.js
@@ -0,0 +1,12 @@
+function one(){
+    let username = 'shubham';
+
+    function two(){
+        let website = 'youtube'
+        console.log(username)
+    }
+    // console.log(website);
+    two()
+}
+
+one();
